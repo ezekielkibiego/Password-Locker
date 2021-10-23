4e9eec00e68cbd211feecd711cbfcae61d128e71
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-10-24 02:27:00 +0300

    added username inptut

--- run.py
+++ run.py
@@ -61,6 +61,20 @@ def generate_password(password_length):
     """
     return Credentials.generate_password(password_length)
 
+def main():
+    print("Hi, What is your name?")
+    name = input()
+    while True:
+        print('*'*50)
+        print("Welcome {name} ...\n kindly, use the following to proceed.\n ca ---  Create New Account  \n lg - to login to your account  \n da -display account \n ex -exit from app")
+        print('*'*50)
+        short_code=input("").upper().strip()
+
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 
