df2c76620aa99d380ab36d5fa043c98cb5326575
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-10-24 01:13:29 +0300

    added find randon account-name

--- details.py
+++ details.py
@@ -14,22 +14,22 @@ class Credentials():
         self.user_account_password = user_account_password
 
     def save_credentials(self):
-        """
-            save credentials method that saves credentials into user_credentials[]
-        """
+        '''
+            Function to save credentials into user_credentials.
+        '''
         Credentials.user_credentials.append(self)
 
     def delete_credentials(self):
-        """
-            deletes saved credential from the user_credentials[]
-        """
+        '''
+            Function to delete saved credential from the user_credentials.
+        '''
         Credentials.user_credentials.remove(self)
 
     @classmethod
     def find_by_account_name(cls, account_name):
-        """
-            Method that takes in a account_platform and returns a credentials that matches that account_platform.
-        """
+        '''
+            Method to take and return credentials that match the account_name.
+        '''
         for credentials in cls.user_credentials:
             if credentials.account_name == account_name:
                 return credentials
@@ -37,15 +37,15 @@ class Credentials():
 
     @classmethod
     def display_credentials(cls):
-        """
-            returns the credentials list(all credentials)
-        """
+        '''
+            Method to return and display the all credentials.
+        '''
         return cls.user_credentials
 
     @classmethod
     def generate_password(cls, password_length):
         '''
-           Method to generate random password for a user creating a new account int the user_credentials
+           Method to generate random password for a user creating a new account in the the user_credentials.
         '''
         alpa = string.ascii_letters + string.digits
         password = ''.join(random.choice(alpa)
