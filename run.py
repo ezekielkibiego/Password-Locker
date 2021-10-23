f0a8bd386c9aa812e3ae901e21d328469af4c32d
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-10-24 01:50:52 +0300

    import user and credentials to run.py file

---
+++ run.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3.8
+from user import User
+from details import Credentials
+
+def create_new_user(username, password):
+    '''
+    create a new user
+    '''
+    new_user = User(username, password)
+    return new_user
+def save_user(user):
+    '''
+    save user
+    '''
+    user.save_user()
+def delete_user(username):
+    '''
+    delete user
+    '''
+    User.delete_user(username)
+def find_existing_user(username):
+    '''
+    find if user exists
+    '''
+    return User.user_exist(username)
+def find_user_password(username, password):
+    '''
+    find if the user enter the correct username and password
+    '''
+    return User.find_user(username, password)
+def create_new_credential(account_name, account_username, account_password):
+    '''
+    create a new credential
+    '''
+    new_credential = Credentials(
+        account_name, account_username, account_password)
+    return new_credential
+def save_credentials(credentials):
+    '''
+    save credentials
+    '''
+    credentials.save_credentials()
+def display_credentials():
+    """
+    display credentials
+    """
+    return Credentials.display_credentials()
+def delete_credential(account_name):
+    '''
+    delete credentials
+    '''
+    return Credentials.delete_credentials(account_name)
+def find_credential(account_name):
+    '''
+    find credentials
+    '''
+    return Credentials.find_by_account_name(account_name)
+def generate_password(password_length):
+    """
+    generate a random password
+    """
+    return Credentials.generate_password(password_length)
+
+
+
+
