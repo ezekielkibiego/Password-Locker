89cfcdc80a53ec884165571dcdb51686b9819002
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-10-23 20:45:56 +0300

    created new files

---
+++ user.py
@@ -0,0 +1,21 @@
+ class user: 
+    """
+    create user class that generates new instances of a user.
+
+    """
+
+    def _init_(self, username, password):
+
+        """
+
+        method used to define the properties of user.
+
+        """
+        self.username = username
+        self.password = password
+
+    def saver_user(self):
+        """
+        
+
+        """
